b29ae9bb76f0875ece7a666466e000743afc4a26
Rio Shen <ryoxshen@gmail.com>
2014-03-23 22:30:39 -0400

    Create map.py

---
+++ map.py
@@ -0,0 +1,8 @@
+import string
+
+original = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm j
+mle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+table = string.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+
+print original.translate(table)
